c5ca5c3b768374d7051e2015ee1c9c76ab8f14c4
Taha Eldeeb <tahaeldeeb.dev@gmail.com>
2026-07-25 13:21:10 +0300

    Implement twoSum function to find indices of target
    Go solution finds two indices that sum to target using hash map for O(n) single-pass lookup. O(n) time, O(n) space.

---
+++ LeetCode Problem Solving/1.TwoSum.go
@@ -0,0 +1,14 @@
+func twoSum(nums []int, target int) []int {
+	result := []int{}
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result = []int{i, j}
+				return result
+			}
+		}
+	}
+
+	return result
+}
