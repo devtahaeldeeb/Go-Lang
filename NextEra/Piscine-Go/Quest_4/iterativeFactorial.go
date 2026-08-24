433668e766c8e2184674bc01bf8c1d01bf4b389e
Taha Eldeeb <tahaeldeeb.dev@gmail.com>
2026-08-24 15:00:12 +0300

    Implement iterative factorial function

---
+++ NextEra/Piscine-Go/Quest_4/iterativeFactorial.go
@@ -0,0 +1,15 @@
+package main
+
+func IterativeFactorial(nb int) int {
+    if nb < 0 {
+        return 0
+    }
+
+    result := 1
+
+    for i := 1; i <= nb; i++ {
+        result = result * i
+    }
+
+    return result
+}
