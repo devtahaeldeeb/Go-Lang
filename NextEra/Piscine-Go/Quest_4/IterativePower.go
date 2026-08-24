5f13bc1f8c5ea44ae402665d159a57f391fadb99
Taha Eldeeb <tahaeldeeb.dev@gmail.com>
2026-08-24 15:15:23 +0300

    Implement IterativePower function for exponentiation

---
+++ NextEra/Piscine-Go/Quest_4/IterativePower.go
@@ -0,0 +1,19 @@
+package main
+
+func IterativePower(nb int, power int) int {
+	if power < 0 {
+		return 0
+	}
+
+	if nb == 0 {
+		return 1
+	}
+
+	result := 1
+
+	for i := 1; i <= power; i++ {
+		result = result * nb
+	}
+
+	return result
+}
