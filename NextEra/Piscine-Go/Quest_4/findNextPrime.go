2cc4dd091b3a998a8e9cc64e3320a2854cc00319
Taha Eldeeb <tahaeldeeb.dev@gmail.com>
2026-08-25 11:22:21 +0300

    Add IsPrime and FindNextPrime functions
    Implement functions to check for prime numbers and find the next prime.

---
+++ NextEra/Piscine-Go/Quest_4/findNextPrime.go
@@ -0,0 +1,25 @@
+package main
+
+func IsPrime(nb int) bool {
+	if nb < 2 {
+		return false
+	}
+
+	for i := 2; i < nb; i++ {
+		if nb%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func FindNextPrime(nb int) int {
+	num := nb
+
+	for !IsPrime(num) {
+		num++
+	}
+
+	return num
+}
